b1af464920a3638afda35b1c101b15ba54327bb0
jennifer yb jones <jennifer.yb.jones@premisehealth.com>
2016-06-02 17:21:28 -0500

    changed data

--- app/providers/people-service/people-service.ts
+++ app/providers/people-service/people-service.ts
@@ -17,7 +17,7 @@ export class PeopleService {
       lastName: "Taylor",
       doctor: "Dr. Randy Pittman",
       appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      dob: "11/14/1983",
       idNumber: "448-21-0891",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/amy_taylor.jpg"
     },
@@ -25,55 +25,60 @@ export class PeopleService {
       id: 2,
       firstName: "Anup",
       lastName: "Gupta",
-      doctor: "Dr. Randy Pittman",
-      appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      doctor: "Dr. Awesome Town",
+      appointmentTime: "09:00 AM",
+      dob: "01/14/1970",
+      idNumber: "555-98-9851",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/anup_gupta.jpg"
     },
     {
       id: 3,
       firstName: "Michael",
       lastName: "Jones",
-      doctor: "Dr. Randy Pittman",
-      appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      doctor: "Dr. Samantha Jones",
+      appointmentTime: "10:00 AM",
+      dob: "09/14/1976",
+      idNumber: "666-66-6666",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michael_jones.jpg"
     },
     {
       id: 4,
       firstName: "Caroline",
       lastName: "Kingsley",
-      doctor: "Dr. Randy Pittman",
-      appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      doctor: "Dr. Kendrick Lamar",
+      appointmentTime: "11:00 AM",
+      dob: "12/14/1970",
+      idNumber: "111-222-0456",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/caroline_kingsley.jpg"
     },
     {
       id: 5,
       firstName: "James",
       lastName: "Kennedy",
-      doctor: "Account Executive",
       doctor: "Dr. Randy Pittman",
-      appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      appointmentTime: "08:30 AM",
+      dob: "11/14/1990",
+      idNumber: "333-21-8976",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/james_kennedy.jpg"
     },
     {
       id: 6,
       firstName: "Jennifer",
       lastName: "Wu",
-      doctor: "Dr. Randy Pittman",
-      appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      doctor: "Dr. Beyonce",
+      appointmentTime: "02:00 PM",
+      dob: "11/14/1991",
+      idNumber: "657-20-8642",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jennifer_wu.jpg"
     },
     {
       id: 7,
       firstName: "Jonathan",
       lastName: "Bradley",
-      doctor: "Dr. Randy Pittman",
-      appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      doctor: "Dr. Jennifer Jones",
+      appointmentTime: "04:00 PM",
+      dob: "11/14/1985",
+      idNumber: "785-42-5555",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jonathan_bradley.jpg"
     },
     {
@@ -81,35 +86,39 @@ export class PeopleService {
       firstName: "Kenneth",
       lastName: "Sato",
       doctor: "Dr. Randy Pittman",
-      appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      appointmentTime: "08:45 AM",
+      dob: "11/14/1990",
+      idNumber: "467-32-0872",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/kenneth_sato.jpg"
     },
     {
       id: 9,
       firstName: "Lisa",
       lastName: "Parker",
-      doctor: "Dr. Randy Pittman",
-      appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      doctor: "Dr. Jim Cipriani",
+      appointmentTime: "11:15 AM",
+      dob: "11/14/1972",
+      idNumber: "434-11-0999",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/lisa_parker.jpg"
     },
     {
       id: 10,
       firstName: "Brad",
       lastName: "Moretti",
-      doctor: "Dr. Randy Pittman",
-      appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      doctor: "Dr. Chelsea Wood",
+      appointmentTime: "08:50 AM",
+      dob: "11/14/1989",
+      idNumber: "763-99-5231",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/brad_moretti.jpg"
     },
     {
       id: 11,
       firstName: "Michelle",
       lastName: "Lambert",
-      doctor: "Dr. Randy Pittman",
-      appointmentTime: "08:00 AM",
-      dob: "11/14/1900",
+      doctor: "Dr. Greg Wiley",
+      appointmentTime: "10:00 AM",
+      dob: "11/14/1950",
+      idNumber: "782-44-0921",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michelle_lambert.jpg"
     },
     {
@@ -119,6 +128,7 @@ export class PeopleService {
       doctor: "Dr. Randy Pittman",
       appointmentTime: "08:00 AM",
       dob: "11/14/1900",
+      idNumber: "319-82-9257",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/miriam_aupont.jpg"
     },
     {
@@ -128,6 +138,7 @@ export class PeopleService {
       doctor: "Dr. Randy Pittman",
       appointmentTime: "08:00 AM",
       dob: "11/14/1900",
+      idNumber: "448-21-0891",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/olivia_green.jpg"
     },
     {
@@ -137,6 +148,7 @@ export class PeopleService {
       doctor: "Dr. Randy Pittman",
       appointmentTime: "08:00 AM",
       dob: "11/14/1900",
+      idNumber: "448-21-0891",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/robert_sullivan.jpg"
     },
     {
@@ -146,6 +158,7 @@ export class PeopleService {
       doctor: "Dr. Randy Pittman",
       appointmentTime: "08:00 AM",
       dob: "11/14/1900",
+      idNumber: "448-21-0891",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/tammy_robinson.jpg"
     },
     {
@@ -155,6 +168,7 @@ export class PeopleService {
       doctor: "Dr. Randy Pittman",
       appointmentTime: "08:00 AM",
       dob: "11/14/1900",
+      idNumber: "448-21-0891",
       picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/victor_ochoa.jpg"
     }
 
